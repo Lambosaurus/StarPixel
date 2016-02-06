07b74f818efb3119812dfd5362025ff74c4606df
Hoppelite <Ginga_ninja03@hotmail.com>
2016-02-06 20:58:39 +1300

    Added stupid stuff

---
+++ StarPixel/IComms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarPixel
+{
+    interface IComms
+    {
+        List<int> Ping(Ship ship);
+
+    }
+}
--- StarPixel/Intellegence.cs
+++ StarPixel/Intellegence.cs
@@ -59,7 +59,6 @@ namespace StarPixel
 
             outputs.control_torque = key_state.IsKeyDown(Keys.E) ? 1.0f : ((key_state.IsKeyDown(Keys.Q)) ? -1.0f : 0.0f);
 
-
             return outputs;
         }
     }
