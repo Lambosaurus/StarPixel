e212e76c9933292730f563e66d880308a4fc2e14
Lambosaurus <tlamborn@gmail.com>
2016-03-02 00:55:38 +1300

    Small value tweaks, far as i can remember.

--- StarPixel/AssetWeaponTemplates.cs
+++ StarPixel/AssetWeaponTemplates.cs
@@ -24,7 +24,7 @@ namespace StarPixel
             template.projectile_color = new Color(1f, 0.9f, 0.5f);
             template.projectile_sprite_resource = "bullet";
             template.projectile_explosion_resource = "boom";
-            template.projectile_scale = new Vector2(1, 0.4f);
+            template.projectile_scale = new Vector2(0.66f, 0.2f);
             template.fire_rate = 10;
             template.projectile_velocity = 6f;
             template.projectile_scatter = 0.5f;
@@ -36,17 +36,17 @@ namespace StarPixel
 
             ArtExplosionResource explosion = new ArtExplosionResource("particle");
 
-            explosion.std_particle_count = 10;
-            explosion.std_particle_length = 0.75f;
+            explosion.std_particle_count = 6;
+            explosion.std_particle_length = 0.55f;
             explosion.std_particle_stretch_width = 1;
-            explosion.std_particle_stretch_length = 6;
-            explosion.std_particle_width = 0.5f;
+            explosion.std_particle_stretch_length = 5;
+            explosion.std_particle_width = 0.35f;
             explosion.std_temperature = 4500;
-            explosion.std_temp_halflife = 0.15f;
+            explosion.std_temp_halflife = 0.1f;
             explosion.std_bounce = 1f;
             explosion.std_scatter = 0.5f;
             explosion.std_particle_size_scatter = 1.5f;
-            explosion.std_particle_life = 0.4f;
+            explosion.std_particle_life = 0.3f;
 
             ArtManager.explosions.Add("boom", explosion);
 
